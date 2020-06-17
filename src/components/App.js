a2d62cf3e25aefa9e19647b6bdbe769ebf62f0f2
kero <kero@comore.bi>
2020-06-17 11:40:08 +0900

    音楽の波形を描画

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import * as utils from '../utils'
 
-const App = () => (
-  <main className="container py-8">
-    <button type="button" className="btn btn-primary" onClick={utils.audio}>
-      再生
-    </button>
-  </main>
-)
+const App = () => {
+  const [bgColor, setBgColor] = useState('#fafafa')
+  const handleBgColor = (e) => setBgColor(e.target.value)
+
+  const [waveColor, setWaveColor] = useState('#22ccdd')
+  const handleWaveColor = (e) => setWaveColor(e.target.value)
+
+  const [src, setSrc] = useState(0)
+
+  const [audioFile, setAudioFile] = useState()
+  const selectAudioFile = (e) => setAudioFile(e.target.files.item(0))
+  useEffect(() => {
+    if (!audioFile) return
+    utils.playAudio({ audioFile, bgColor, waveColor, src, setSrc })
+  }, [audioFile, bgColor, waveColor])
+
+  return (
+    <main className="py-10 row">
+      <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+        <section className="mb-8">
+          <div className="form-group form-row">
+            <label className="col-sm-2 col-form-label">背景色</label>
+            <div className="col-sm-9">
+              <input type="text" className="form-control" defaultValue={bgColor} onChange={handleBgColor} />
+            </div>
+            <div className="col-sm-1">
+              <div style={{ backgroundColor: bgColor }} className="w-100 h-100 border" />
+            </div>
+          </div>
+          <div className="form-group form-row">
+            <label className="col-sm-2 col-form-label">描画色</label>
+            <div className="col-sm-9">
+              <input type="text" className="form-control" defaultValue={waveColor} onChange={handleWaveColor} />
+            </div>
+            <div className="col-sm-1">
+              <div style={{ backgroundColor: waveColor }} className="w-100 h-100 border" />
+            </div>
+          </div>
+          <div className="form-group form-row">
+            <div className="col-sm-2 col-form-label">ファイル</div>
+            <div className="col-sm-10">
+              <div className="custom-file">
+                <input id="select-audio-file" type="file" className="custom-file-input" onChange={selectAudioFile} />
+                <label className="custom-file-label" htmlFor="select-audio-file">
+                  <i className="fa upload mr-1" />
+                  {(audioFile && audioFile.name) || 'ファイルを選択'}
+                </label>
+              </div>
+            </div>
+          </div>
+        </section>
+        <canvas id="visualizer" className="visualiser" height="640" />
+      </div>
+    </main>
+  )
+}
 
 export default App
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,55 @@
-export const audio = () => {
-  const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+export const playAudio = ({ audioFile, bgColor, waveColor, src, setSrc }) => {
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)()
+  const fileReader = new FileReader()
+  let animationId
 
-  const oscillatorNode = audioCtx.createOscillator()
-  const gainNode = audioCtx.createGain()
-  gainNode.gain.value = 0.5
-  oscillatorNode.connect(gainNode)
-  gainNode.connect(audioCtx.destination)
-  oscillatorNode.start(0)
+  const analyser = audioContext.createAnalyser()
+  analyser.fftSize = 128
+  analyser.connect(audioContext.destination)
+
+  const canvas = document.getElementById('visualizer')
+  const canvasContext = canvas.getContext('2d')
+  canvas.setAttribute('width', 850)
+  canvas.setAttribute('height', 640)
+
+  fileReader.onload = () => {
+    audioContext.decodeAudioData(fileReader.result, (buffer) => {
+      if (src) {
+        src.stop()
+        cancelAnimationFrame(animationId)
+      }
+
+      const tempSrc = audioContext.createBufferSource()
+      setSrc(tempSrc)
+
+      tempSrc.buffer = buffer
+      tempSrc.connect(analyser)
+      tempSrc.start(0)
+
+      animationId = requestAnimationFrame(render)
+    })
+  }
+  fileReader.readAsArrayBuffer(audioFile)
+
+  const render = () => {
+    const spectrums = new Uint8Array(analyser.frequencyBinCount)
+    const interval = canvas.width / spectrums.length
+
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height)
+
+    canvasContext.fillStyle = bgColor
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+
+    canvasContext.fillStyle = waveColor
+    analyser.getByteFrequencyData(spectrums)
+    for (let i = 0; i < spectrums.length; i++) {
+      canvasContext.fillRect(i * interval, canvas.height - spectrums[i] * 2, interval - 2, spectrums[i] * 2)
+    }
+
+    canvasContext.textAlign = 'right'
+    canvasContext.font = "20px 'sans-serif'"
+    canvasContext.fillText(audioFile.name, canvas.width - 20, 40)
+
+    animationId = requestAnimationFrame(render)
+  }
 }
